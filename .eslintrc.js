8ad61206f8fa3964af5a8c80433a15828287e376
陶静波 <taojingbo02@meituan.com>
2021-08-06 15:38:13 +0800

    错误b

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,6 @@ module.exports = {
         // vue style guide 推荐的规则集
         // @see https://github.com/vuejs/eslint-plugin-vue/blob/v6.2.2/docs/rules/README.md
         'plugin:vue/recommended',
-        // standard JavaScript 规则集
-        // @see https://github.com/vuejs/eslint-plugin-vue/blob/v6.2.2/docs/rules/README.md
-        // 关闭所有和 prettier 冲突的规则检查。总是置于最后
-        // @see https://github.com/prettier/eslint-config-prettier#installation
-        'prettier',
-        'prettier/vue',
     ],
     rules: {
         'vue/attributes-order': 0,
