d6dbbbc1c82c269d2dd68fa23c1399496e8728f3
陶静波 <taojingbo02@meituan.com>
2021-08-06 15:41:20 +0800

    错误cdd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 // .eslintrc.js 
 module.exports = {
-    extends: [
-        // vue style guide 推荐的规则集
-        // @see https://github.com/vuejs/eslint-plugin-vue/blob/v6.2.2/docs/rules/README.md
-        'plugin:vue/recommended',
-    ],
-    rules: {
-        'vue/attributes-order': 0,
-        'vue/order-in-components': 0,
+    "root": true,
+    "env": {
+      "node": true
     },
+    "extends": [
+      "plugin:vue/vue3-essential",
+      "eslint:recommended"
+    ],
+    "parserOptions": {
+      "parser": "babel-eslint"
+    },
+    "rules": {}
 };
